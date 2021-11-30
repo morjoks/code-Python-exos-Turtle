bdeab6e89af4ca62f055c5359cda6c3c18723192
morjoks <93184448+morjoks@users.noreply.github.com>
2021-11-30 09:21:55 +0100

    Add files via upload

---
+++ algo tri insertion.py
@@ -0,0 +1,9 @@
+def tri_insertion(L):
+    N = len(L)
+    for n in range(1,N):
+        cle = L[n]
+        j = n-1
+        while j>=0 and L[j] > cle:
+            L[j+1] = L[j]
+            j = j-1
+        L[j+1] = cle
